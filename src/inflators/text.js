e0f01d60ef0e10e695f34341ef2d58b9065e5819
Urs Hunkler <urs.hunkler@unodo.de>
2024-03-18 16:49:30 +0100

    Troika text (#24)
    * first troika text impelementation
    
    * check if troika-text is registered
    
    * change name to text, remove dist
    
    * remove dist from .gitignore
    
    * add dist again
    
    * move the »text« inflator related lines up into the alphabetic order

---
+++ src/inflators/text.js
@@ -0,0 +1,69 @@
+import { createEntityAndReparent } from "./utils";
+
+export function inflateText(node, componentProps, otherComponents) {
+  const componentName = "troika-text";
+  if (!AFRAME.components[componentName]) {
+    console.error(`Component ${componentName} not registered`);
+    return;
+  }
+
+  const el = createEntityAndReparent(node);
+
+  // Rename the Hubs properties for the baseline settings.
+  switch (componentProps.anchorY) {
+    case 'middle':
+      componentProps.anchorY = 'center';
+      break;
+
+    case 'bottom':
+    case 'bottom-baseline':
+      componentProps.anchorY = 'bottom';
+      break;
+
+    case 'top':
+    case 'top-baseline':
+      componentProps.anchorY = 'top';
+      break;
+  }
+
+  // Set different property names.
+  let textProps = Object.assign({
+    align: componentProps.textAlign,
+    anchor: componentProps.anchorX,
+    baseline: componentProps.anchorY,
+  }, componentProps);
+
+  // Remove properties not handled or different in A-Frame troika-text.
+  delete textProps.textAlign;
+  delete textProps.anchorX;
+  delete textProps.anchorY;
+  delete textProps.anchorY;
+  delete textProps.opacity;
+  delete textProps.side;
+
+  el.setAttribute("troika-text", textProps);
+
+  // Set opacity if the value is not »1.0«.
+  // The "troika-text-material" setAttribute only works
+  // when the attribute is a string. The object creates an error.
+  let opacityProp = '';
+  if (componentProps.opacity !== 1.0) {
+    opacityProp = "opacity: " + componentProps.opacity + "; transparent: true;";
+  }
+
+  el.setAttribute("troika-text-material",
+    "side: " + componentProps.side + "; " +
+    opacityProp);
+
+  // This code throws an error.
+  // const materialProps = {side: componentProps.side};
+  // if (componentProps.opacity !== 1.0) {
+  //   materialProps.opacity = componentProps.opacity
+  //   materialProps.transparent = true
+  // }
+
+  // el.setAttribute("troika-text-material", materialProps);
+  // el.setAttribute("material", materialProps);
+
+  return el;
+}
