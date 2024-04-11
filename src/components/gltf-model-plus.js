d40ebca982968916eabe7fa3171ad4c52226a817
Vincent Fretin <vincent.fretin@gmail.com>
2024-04-11 19:48:27 +0200

    Clear the cache after successfully loaded the glb to free some memory

--- src/components/gltf-model-plus.js
+++ src/components/gltf-model-plus.js
@@ -380,6 +380,8 @@ export const gltfModelPlus = {
           // el could be a FakeEntity if we aliased gtlf-model to gtlf-model-plus, so we check classList is not undefined
           if (el.classList?.contains("environment-settings")) {
             gltfInflators.get("environment-settings")(el.sceneEl, environmentSettings);
+            // Clear the cache after successfully loaded the glb to free some memory
+            THREE.Cache.clear();
           }
 
           setTimeout(() => {
